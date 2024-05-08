f9a862f4c075c27658d7a732449ec935288c7399
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2024-05-08 23:06:42 +0400

    reslove chakra

--- pages/admin/restaurants.tsx
+++ pages/admin/restaurants.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '@/shared/AdminComponents/Header';
 import {
   InputGroup,
@@ -7,61 +8,49 @@ import {
   Box,
   InputRightElement,
 } from '@chakra-ui/react';
-import React from 'react';
 
 const Restaurants = () => {
   return (
-
-    <Box
-      bg='#27283C'
-      minHeight='100vh'
-      display='flex'
-      flexDirection='column'
-    >
-
-    <Box className=' bg-darkBlue10' minHeight="100vh" display="flex" flexDirection="column"> 
-
-      <Header />
-      <Box
-        display='flex'
-        justifyContent='flex-end'
-        p='10'
-      >
-        <Box
-          bg='#27283C'
-          borderWidth='0.5px'
-          borderRadius={16}
-          top={80}
-          width={1108}
-          height={73}
-        >
-          <Text color='white'>Restaurants</Text>
-          <InputGroup
-            display='flex'
-            justifyContent='flex-end'
-            alignItems='center'
+    <Box bg='#27283C' minHeight='100vh' display='flex' flexDirection='column'>
+      <Box className='bg-darkBlue10' minHeight="100vh" display="flex" flexDirection="column"> 
+        <Header />
+        <Box display='flex' justifyContent='flex-end' p='10'>
+          <Box
+            bg='#27283C'
+            borderWidth='0.5px'
+            borderRadius={16}
+            top={80}
+            width={1108}
+            height={73}
           >
-            <Select
-              bgColor='#5A5B70'
-              borderRadius={20}
-              width={200}
-              height={35}
-              placeholder='Select category'
-              mr='2'
+            <Text color='white'>Restaurants</Text>
+            <InputGroup
+              display='flex'
+              justifyContent='flex-end'
+              alignItems='center'
             >
-              <option value='breakfast'>Breakfast</option>
-              <option value='lunch'>Lunch</option>
-              <option value='dinner'>Dinner</option>
-            </Select>
-            <Box>
-              <Button
+              <Select
+                bgColor='#5A5B70'
                 borderRadius={20}
-                colorScheme='pink'
+                width={200}
+                height={35}
+                placeholder='Select category'
+                mr='2'
               >
-                + ADD RESTAURANTS
-              </Button>
-            </Box>
-          </InputGroup>
+                <option value='Papa Johns'>Papa John's</option>
+                <option value='Burger King'>Burger King</option>
+                <option value='Mcdonalds'>McDonald's</option>
+              </Select>
+              <Box>
+                <Button
+                  borderRadius={20}
+                  colorScheme='pink'
+                >
+                  + ADD RESTAURANTS
+                </Button>
+              </Box>
+            </InputGroup>
+          </Box>
         </Box>
       </Box>
     </Box>
