c73ac5172ffa6e37d7eebe6c5f25e04a00810ab5
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-10 18:28:12 +0400

    progressBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import { PagesProgressBar as ProgressBar } from 'next-nprogress-bar';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <ProgressBar height='4px' color='#fffd00'/>
+
     </ChakraProvider>
   )
 }
