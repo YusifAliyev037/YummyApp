bcbe7865f7a0268b9276256401f221522e5b092f
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2024-05-10 22:48:49 +0400

    created form

--- pages/admin/restaurants.tsx
+++ pages/admin/restaurants.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from '@/shared/AdminComponents/Header';
 import {
   InputGroup,
@@ -6,12 +5,10 @@ import {
   Select,
   Text,
   Box,
-  InputRightElement,
   Card,
   CardBody,
   Image,
   Stack,
-  Heading,
   Divider,
   CardFooter,
   ButtonGroup,
@@ -19,9 +16,15 @@ import {
 import PushModul from '@/shared/AdminComponents/PushModul';
 import MetaSeo from '@/shared/MetaSeo';
 import Head from 'next/head';
-
+import Modul from '@/shared/AdminComponents/Modul';
+import React, { useState } from 'react';
 
 const Restaurants = () => {
+    const [showModul, setShowModul] = useState(false);
+
+    const handleAddRestaurantClick = () => {
+      setShowModul(true);
+    };
   return (
     <Box
       className='bg-darkBlue10'
@@ -40,20 +43,25 @@ const Restaurants = () => {
       </Head>
       <Header />
 
-      <Box className='flex my-4 mx-8'>
+      <Box className='flex  mx-8 '>
         <PushModul />
-
+        {showModul && <Modul />}
         <Box
           bg='#27283C'
-          className='flex items-center w-5/6 px-8 '
+          className='flex items-center w-5/6 px-8 mt-20'
           borderRadius={16}
           height={73}
         >
-          <Text color='white'>Restaurants</Text>
-          <InputGroup className='flex justify-end items-center gap-7'>
+          <Text
+            as='p'
+            color='white'
+          >
+            Restaurants
+          </Text>
+          <InputGroup className='flex justify-end items-center  gap-7'>
             <Select
               bgColor='#5A5B70'
-              borderRadius={20}
+              borderRadius={14}
               width={200}
               height={35}
               placeholder='Select category'
@@ -65,19 +73,16 @@ const Restaurants = () => {
             </Select>
             <Box>
               <Button
-                borderRadius={20}
+                borderRadius={14}
                 colorScheme='pink'
-
+                onClick={handleAddRestaurantClick}
               >
                 + ADD RESTAURANTS
               </Button>
             </Box>
           </InputGroup>
         </Box>
-      </Box>
-
-      <Box className='flex  items-center w-60 gap-7'>
-        <Card>
+        <Card className='w-60'>
           <CardBody>
             <Image
               className='w-full'
@@ -109,6 +114,9 @@ const Restaurants = () => {
           </CardFooter>
         </Card>
       </Box>
+
+     
+   
     </Box>
   );
 };
