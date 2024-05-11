ad54c2a04ef7f93c6d118b571a72f8e5d66b62af
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-12 00:54:42 +0400

    authInProgress

--- shared/AdminComponents/Services/axios.ts
+++ shared/AdminComponents/Services/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const baseURL = "/api"
 
@@ -6,6 +7,27 @@ const baseURL = "/api"
 const instanceAxios = axios.create({
     baseURL,
     headers:{
-        Accept:"application/json"
+        Accept:"application/json",
+        "Content-Type":"application/json",
     }
-})
+});
+
+export type FormRegister = {
+    email: string | undefined;
+    password: string | undefined;
+    fullname?: string | undefined;
+    username?: string | undefined;
+};
+
+//! LOGIN
+
+export const completeLogin = async (form:FormRegister) => {
+    try{
+        const response = await instanceAxios.post("/auth/signin", form);
+        return response
+    }catch(error){
+       toast.warning("Chek your information!");
+       console.log(error);
+        
+    }
+}
