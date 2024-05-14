c2171b2713ddac64f46664da244818b772422640
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2024-05-14 20:15:12 +0400

    I connected to the restaurant api restaurant page

--- shared/AdminComponents/Services/axios.ts
+++ shared/AdminComponents/Services/axios.ts
@@ -1,66 +1,62 @@
-import axios from "axios";
-import { error } from "console";
-const baseURL = "/api"
+import axios from 'axios';
 
-
+const baseURL = '/api';
 
 const instanceAxios = axios.create({
-    baseURL,
-    headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-    }
+  baseURL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
 
 export type Form = {
-    name: string | undefined;
-    slug: string |undefined;
-    img_url: string | undefined
-  
-}
-
-
+  name: string | undefined;
+  slug: string | undefined;
+  img_url: string | undefined;
+};
 
 export type FormRegister = {
-    email: string | undefined;
-    password: string | undefined;
-    fullname?: string | undefined;
-    username?: string | undefined;
+  email: string | undefined;
+  password: string | undefined;
+  fullname?: string | undefined;
+  username?: string | undefined;
 };
 
 //! LOGIN
 
-export const completeLogin = async (form:FormRegister) => {
-    try{
-        const response = await instanceAxios.post("/auth/signin", form);
-        return response
-    }catch(error){
-        alert("Chek your infomation!")
-   
-       console.log(error);
-        
-    }
-}
+export const completeLogin = async (form: FormRegister) => {
+  try {
+    const response = await instanceAxios.post('/auth/signin', form);
+    return response;
+  } catch (error) {
+    alert('Chek your infomation!');
 
+    console.log(error);
+  }
+};
 
 // //!category
+export async function getCategories() {
+    try {
+      const response = await instanceAxios.get('/category');
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+export type Restaurant = {
+  id?: number;
+  name?: string;
+};
 
-
-
-// export const category = async( form:FormCategory) =>{
-//     try{const response = await instanceAxios.post("/category")}
-// }
-
-
-
-export async function getCategories () {
-    try{const response = await instanceAxios.get("/category")
-
+export async function getRestaurants() {
+  try {
+    const response = await instanceAxios.get('/restaurants');
     return response
-
-    }catch{error}(
-        console.log(error)
-        
-    )
+  } catch (error) {
+    console.error('Error while fetching restaurants:', error);
+    throw new Error('Failed to fetch restaurants!');
+  }
 }
-
