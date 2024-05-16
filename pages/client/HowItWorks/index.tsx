cc4fb8d56127beeef90cad5a96928279cc352f21
MuraddAlizada <alizademurad@hotmail.com>
2024-05-16 14:12:28 +0400

    client-pages

---
+++ pages/client/HowItWorks/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import ClientHeader from "@/components/ClientHeader";
+
+const HOWITWORKS = () => {
+  
+
+  return (
+    
+    <>
+    <ClientHeader/>
+  
+  </>
+
+  );
+};
+
+export default HOWITWORKS;
