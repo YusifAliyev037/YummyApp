eb0bc8f87d5319dec51641e0db24ba1c38490424
MuraddAlizada <alizademurad@hotmail.com>
2024-05-16 21:08:42 +0400

    creating faq page

--- pages/client/Faqs/index.tsx
+++ pages/client/Faqs/index.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 import axios from "axios";
 import ClientHeader from "@/components/ClientHeader";
+import SectionFaq from "@/pages/client/Faqs/Section/index";
 
 const Faqs = () => {
   return (
     <>
       <ClientHeader />
+      <SectionFaq />
     </>
   );
 };
