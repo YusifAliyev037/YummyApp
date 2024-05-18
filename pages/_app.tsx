05b8d1b0331462c73f390e17ceca2241457c4dba
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-18 13:27:06 +0400

    reduxProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,20 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import { PagesProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { Provider } from 'react-redux';
+import { store } from '@/shared/redux/store';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider store={store}>
+
     <ChakraProvider>
       <Component {...pageProps} />
       <ProgressBar height='4px' color='#8b1fab'/>
 
     </ChakraProvider>
+    </Provider>
   )
 }
 
