3e0de797bdbb1a177dc660a6af3e5077b547a83e
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-22 18:37:37 +0400

    headerResponsiveDoneIn80%

--- shared/AdminComponents/PushModul.tsx
+++ shared/AdminComponents/PushModul.tsx
@@ -3,13 +3,24 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-function PushModul() {
+interface Props {
+    hidden?: boolean;
+    setHidden?:React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const PushModul: React.FC<Props> = ({
+    hidden,
+    setHidden
+}) => {
+
     const { push, asPath } = useRouter();
 
     const isActive = (path:string) => (asPath === path ? "#d25ff5" : "none");
 
     return (
-        <Box as='section' className='mt-20 bg-pink10 w-64 rounded-xl h-96 p-5 m-5 hidden md:block '>
+        <Box as='section' className={`mt-20 bg-pink10 w-64 rounded-xl h-96 p-5 m-5 hidden md:block ${
+            hidden ? "hidden" : "fixed"
+          }`}>
             <Box as="ul" className=' w-64 flex flex-col  '>
                 <Button
                     onClick={() => push("/admin")}
