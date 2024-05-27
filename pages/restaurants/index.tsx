ebd49ded34aa83ff226b950ab10d904ca55436a8
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-27 15:23:03 +0400

    restaurantInProgress

---
+++ pages/restaurants/index.tsx
@@ -0,0 +1,18 @@
+
+import ClientFooter from '@/shared/ClientComponent/ClientFooter';
+import ClientHeader from "@/shared/ClientComponent/ClientHeader";
+import RestaurantNavbar from '@/shared/ClientComponent/RestaurantNavbar';
+
+const Restaurant = () => {
+  return (
+    <>
+      <ClientHeader />
+    <RestaurantNavbar/>
+
+
+      <ClientFooter />
+    </>
+  );
+};
+
+export default Restaurant;
---
+++ shared/ClientComponent/RestaurantNavbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RestaurantNavbar() {
+  return (
+    <div>restaurantNavbar</div>
+  )
+}
+
+export default RestaurantNavbar
