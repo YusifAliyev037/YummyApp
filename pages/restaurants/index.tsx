54fcd32e66aa8156d17193b71e4baebeca9345c0
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-28 20:01:43 +0400

    restaurantCardFinished

--- pages/restaurants/index.tsx
+++ pages/restaurants/index.tsx
@@ -3,14 +3,24 @@ import ClientHeader from '@/shared/ClientComponent/ClientHeader'
 import ClientFooter from '@/shared/ClientComponent/ClientFooter'
 import React from 'react'
 import RestaurantNavbar from '@/shared/ClientComponent/RestaurantNavbar'
+import RestaurantCard from '@/shared/ClientComponent/RestaurantCard'
+import { Box } from '@chakra-ui/react'
 
 function index() {
   return (
     <>
+    <Box>
+        <Box as='header'>
         <ClientHeader/>
+        </Box>
+        <Box as='main' className=' flex'>
         <RestaurantNavbar/>
-        
+        <RestaurantCard/>
+        </Box>
+        <Box as='footer'>
         <ClientFooter/>
+        </Box>
+    </Box>
       
     </>
   )
