136d7ce361600700f9f34da09e1452c23f28b44d
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-05-29 02:40:56 +0400

    APIRestaurantCategoryFinishedSuccessFully

--- shared/ClientComponent/RestaurantNavbar.tsx
+++ shared/ClientComponent/RestaurantNavbar.tsx
@@ -1,52 +1,62 @@
-
 import { Box, Button } from '@chakra-ui/react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { CategoryType } from '../AdminComponents/TableCategory';
+import { getCategories } from '../AdminComponents/Services/axios';
 
 function RestaurantNavbar() {
-    const {  asPath } = useRouter();
+    const { asPath } = useRouter();
+    const [categories, setCategories] = useState<CategoryType[]>([]);
 
     const isActive = (path: string) => (asPath === path ? '#FFB5A5' : 'none');
   
-    const category: CategoryType[] = useSelector((state: any) => state.global.category);
-    console.log(category);
+    const fetchCategories = async () => {
+        try {
+            const res = await getCategories();
+            const categoryArr = res?.data.result.data;
+            setCategories(categoryArr);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    
+    useEffect(() => {
+        fetchCategories();
+    }, []);
     
     return (
-      <Box
-        as='section'
-        className='bg-white40 rounded-xl p-5 m-7 mt-4 '
-        width='250px'
-        height='650px'
-      >
-        <Box as='ul' className='flex flex-col mt-8'>
-          <Box mb='26px'>
-          {category.map((item, index) =>(
-                
-           
-          <Button
-            key={index}
-            className='flex gap-4 w-52 text-black300 cursor-pointer hover:bg-redUserModul10 hover:w-52'
-            color='text-redUserModul'
-            style={{
-              justifyContent: 'flex-start',
-              backgroundColor: isActive('/user'),
-            }}
-            as='li'
-          >
-            <Image width={18} alt='profile' height={18} src='/rest.svg' />
-            {item.name}
-          </Button>
-        ))}
-          </Box>
-       
-          
+        <Box
+            as='section'
+            bg='whiteAlpha.40'
+            borderRadius='xl'
+            p={5}
+            m={7}
+            mt={4}
+            width='250px'
+            height='650px'
+        >
+            <Box as='ul' mt={8} display='flex' flexDirection='column'>
+                {categories?.map((item, index) => (
+                    <Button
+                        key={index}
+                        className='flex gap-4 w-52 text-black300 cursor-pointer hover:bg-redUserModul10 hover:w-52'
+                        color='text-redUserModul'
+                        style={{
+                            justifyContent: 'flex-start',
+                            backgroundColor: isActive('/user'),
+                            marginBottom: '26px',
+                        }}
+                        as='li'
+                    >
+                        <Image width={18} alt='profile' height={18} src='/rest.svg' />
+                        {item.name}
+                    </Button>
+                ))}
+            </Box>
         </Box>
-      </Box>
     );
-
 }
 
-export default RestaurantNavbar
+export default RestaurantNavbar;
