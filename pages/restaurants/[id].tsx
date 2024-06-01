37f240d0a0cf8259cf29e920b87dba81522ae824
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-06-02 03:42:36 +0400

    restaurantIdPageInProgress

--- pages/restaurants/[id].tsx
+++ pages/restaurants/[id].tsx
@@ -1,32 +1,76 @@
-import ClientHeader from '@/shared/ClientComponent/ClientHeader'
-import ClientFooter from '@/shared/ClientComponent/ClientFooter'
-import MetaSeo from '@/shared/MetaSeo'
-import { Box, Image } from '@chakra-ui/react'
-import Head from 'next/head'
-import React from 'react'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getEditRestaurant, Restaurant } from '@/shared/AdminComponents/Services/axios';
+import { Box, Text, Image } from '@chakra-ui/react';
+import ClientHeader from "@/shared/ClientComponent/ClientHeader";
+import ClientFooter from "@/shared/ClientComponent/ClientFooter";
 
-function id() {
+const RestaurantPage: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query as { id: string };
+  const [restaurant, setRestaurant] = useState<Restaurant | null>(null);
   
-  return (
-    <Box>
-      <Box>
-      <Head>
-          <title>Products</title>
-          <MetaSeo title="Restaurant id" desc="Products Page" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <ClientHeader/>
-      </Box>
-      <Box as='section'>
-        <Image
-        src='https://www.papajohns.az/img/content/pj_logo_web_new.png'
-        />
-      </Box>
-      <Box as='section'>salammm</Box>
-      <Box as='section'></Box>
-      <ClientFooter/>
-    </Box>
-  )
-}
+  useEffect(() => {
+    if (id) {
+      const fetchRestaurant = async () => {
+        try {
+          const res = await getEditRestaurant(id);
+          setRestaurant(res?.data.result);
+        } catch (error) {
+          console.error('Error fetching restaurant:', error);
+        }
+      };
+      fetchRestaurant();
+    }
+  }, [id]);
 
-export default id
+  if (!restaurant) {
+    return <div>Loading...</div>;
+  }
+
+  // Ensure restaurant is not null before accessing its properties
+  const restaurantEntries = restaurant ? Object.entries(restaurant) : [];
+  console.log(restaurantEntries);
+  
+
+  return (
+    <>
+      <ClientHeader />
+      <Box className=' px-8'>
+        {restaurantEntries.map((item, index)=>(
+          <Box key={index}>
+        <Image className=' w-full h-[450px] object-cover' src={item[1].img_url} alt={item[1].name} />
+          <Box className='flex justify-around'> 
+            <Box>
+              <Text className=' font-bold text-xl text-gray600'  >{item[1].name}</Text>
+              <Text className=' font-medium text-sm text-gray300'>{item[1].address}</Text>
+            </Box>
+            <Box className=' flex gap-5'>
+            <Box >
+              <Text className=' text-gray300 font-medium text-lg'>Cusine</Text>
+              <Text className=' text-gray300 font-medium text-sm'>{item[1].cuisine}</Text>
+            </Box>
+            <Box className=' border border-2 border-red400 rounded-md p-1 px-2'>
+              <Text className=' font-normal text-sm text-red400'>${item[1].delivery_price} </Text>
+              <Text className=' font-normal text-red400'>Delivery</Text>
+            </Box>
+            <Box className=' border border-2 border-red400 bg-red400 rounded-md p-1 px-2'>
+              <Text 
+              className=' text-white cursor-pointer text-left'
+              onClick={()=> router.push("/restaurants")}
+              >
+                Go Back</Text>
+            </Box>
+            </Box>
+           
+
+          </Box>
+          </Box>
+        ))}
+      </Box>
+      <ClientFooter />
+    </>
+  );
+};
+
+export default RestaurantPage;
--- pages/restaurants/index.tsx
+++ pages/restaurants/index.tsx
@@ -1,19 +1,116 @@
 import ClientHeader from "@/shared/ClientComponent/ClientHeader";
 import ClientFooter from "@/shared/ClientComponent/ClientFooter";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Card, CardBody, Image, Stack, Text, useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Restaurant, getRestaurants } from "@/shared/AdminComponents/Services/axios";
 import RestaurantNavbar from "@/shared/ClientComponent/RestaurantNavbar";
-import RestaurantCard from "@/shared/ClientComponent/RestaurantCard";
-import { Box } from "@chakra-ui/react";
 
-function index() {
+const Index: React.FC = () => {
+  const router = useRouter();
+  const toast = useToast();
+  const [restaurant, setRestaurant] = useState<Restaurant[]>([]);
+  const [filteredRes, setFilteredRes] = useState<Restaurant[]>([]);
+
+  const filterRes = (id?: string) => {
+    if (id) {
+      const filteredArr = restaurant.filter((item) => item.category_id === id);
+
+      if (filteredArr.length === 0) {
+        toast({
+          title: "Can't find any product!",
+          status: "warning",
+          duration: 2000,
+          position: "top-right",
+          variant: "subtle",
+        });
+      }
+      setFilteredRes(filteredArr);
+    } else {
+      setFilteredRes(restaurant);
+    }
+  };
+
+  const fetchRestaurants = async () => {
+    try {
+      const res = await getRestaurants();
+      const restaurantArr = res?.data.result.data;
+      setRestaurant(restaurantArr);
+    } catch (error) {
+      console.error("Error fetching restaurants:", error);
+    }
+  };
+
+  const handleClickCard = (id: string) => {
+    router.push(`/restaurants/${id}`);
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  useEffect(() => {
+    filterRes();
+  }, [restaurant]);
+
   return (
     <>
       <Box>
         <Box as="header">
           <ClientHeader />
         </Box>
-        <Box as="main" className=" flex">
-          <RestaurantCard />
+        <Box as="main">
+          <Box className="flex">
+            <Box>
+              <RestaurantNavbar onClick={filterRes} />
+            </Box>
+            <Box className="flex gap-10 flex-wrap">
+              {filteredRes.map((item) => (
+                <Card
+                  className="mt-6 h-80 w-58 cursor-pointer"
+                  key={item.id}
+                  onClick={() => handleClickCard(item.id)}
+                >
+                  <CardBody>
+                    <Image
+                      src={item.img_url}
+                      alt={item.name}
+                      className="object-cover"
+                      borderRadius="sm"
+                      height={"160px"}
+                      width={"170px"}
+                    />
+                    <Stack height="100%">
+                      <Text
+                        className="mt-2 text-gray600 font-medium text-xl"
+                        fontFamily={"Roboto"}
+                        lineHeight={"20px"}
+                      >
+                        {item.name}
+                      </Text>
+                      <Text
+                        className="font-medium text-sm text-gray300 mb-2"
+                        fontFamily={"Roboto"}
+                      >
+                        {item.cuisine}
+                      </Text>
+                      <Box className="flex items-center gap-3">
+                        <Text
+                          className="text-gray600 font-medium text-base"
+                          fontFamily={"Roboto"}
+                        >
+                          ${item.delivery_price} Delivery
+                        </Text>
+                        <Text className="bg-red400 py-1 px-3 rounded-30 text-white text-base font-medium">
+                          {item.delivery_min} Min
+                        </Text>
+                      </Box>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              ))}
+            </Box>
+          </Box>
         </Box>
         <Box as="footer">
           <ClientFooter />
@@ -21,6 +118,6 @@ function index() {
       </Box>
     </>
   );
-}
+};
 
-export default index;
+export default Index;
