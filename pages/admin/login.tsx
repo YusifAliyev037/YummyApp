fab5afbb232f094553a4ff34171e2e2e7663dc58
MuraddAlizada <alizademurad@hotmail.com>
2024-06-03 00:11:54 +0400

    fix language menu all pages

--- pages/admin/login.tsx
+++ pages/admin/login.tsx
@@ -7,14 +7,17 @@ import { Box, Text, useToast } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
-import { translate } from '@/public/lang/translate';
+import React, { useRef, useState } from "react";
+import { translate } from "@/public/lang/translate";
 
 function Adminlogin() {
-  const router=useRouter()
+  const router = useRouter();
   const toast = useToast();
 
-  const { push,locale } = useRouter();
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState<string>("en");
+
+  const { push, locale } = useRouter();
 
   const emailRef = useRef<HTMLInputElement>(null);
 
@@ -104,13 +107,24 @@ function Adminlogin() {
 
     return;
   }
-  const changeLanguage = (locale: string) => {
-    router.push(router.pathname, router.asPath, { locale });
-    localStorage.setItem('lang', locale);
 
+  const changeLanguage = (locale: string) => {
+    setSelectedLanguage(locale);
+    router.push(router.pathname, router.asPath, { locale });
+    localStorage.setItem("lang", locale);
+    setShowDropdown(false);
   };
 
- 
+  const languageFlagMap: { [key: string]: string } = {
+    en: "/usuk.png",
+    az: "/azerbaijan.png",
+    fr: "/russian.png",
+  };
+
+  const availableLanguages = Object.keys(languageFlagMap).filter(
+    (lang) => lang !== selectedLanguage
+  );
+
   return (
     <Box className=" bg-darkBlue10  pb-[330px]">
       <Head>
@@ -134,7 +148,7 @@ function Adminlogin() {
           className="bg-none md:bg-darkblue30 w-full md:w-1/3 h-auto md:h-96 flex justify-center gap-5 flex-col p-6 md:p-11"
         >
           <Text as="h2" className=" text-4xl text-center text-gray10 font-bold">
-          {translate("Welcome Admin", locale)}
+            {translate("Welcome Admin", locale)}
           </Text>
           <input
             ref={emailRef}
@@ -152,21 +166,40 @@ function Adminlogin() {
             onClick={login}
             className=" bg-pink rounded py-3 text-white text-2xl font-medium"
           >
-          {translate("Sign In", locale)}
+            {translate("Sign In", locale)}
           </button>
         </Box>
         <Box
           as="section"
           className="bg-none md:bg-white20 w-full md:w-1/3 h-auto md:h-96 p-4"
         >
-          <Image
-            className="hidden md:block m-2 float-end cursor-pointer"
-            width={40}
-            height={40}
-            alt="eng"
-            src="/eng.svg"
-            onClick={()=>changeLanguage('en')}
-          />
+          <div className="relative flex items-center mr-8">
+            <div
+              className={`cursor-pointer flex items-center ${
+                showDropdown ? "active" : ""
+              }`}
+              onClick={() => setShowDropdown(!showDropdown)}
+            >
+              <img
+                src={languageFlagMap[selectedLanguage]}
+                alt={selectedLanguage}
+                className="w-12 h-10 rounded-full transition-transform transform hover:scale-110"
+              />
+              {showDropdown && (
+                <div className="absolute top-full left-0 mt-2 p-2 bg-gray200 border border-black rounded-md z-50">
+                  {availableLanguages.map((lang) => (
+                    <img
+                      key={lang}
+                      src={languageFlagMap[lang]}
+                      alt={lang}
+                      className="w-12 h-10 rounded-full mb-2"
+                      onClick={() => changeLanguage(lang)}
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
           <Box className="flex items-center justify-center mt-4 md:mt-9">
             <Image
               width={200}
