2a3e4cd5da02fac47a7b97cedd530c568f87c9c0
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2024-06-03 20:13:42 +0400

    test4

--- public/lang/translate.js
+++ public/lang/translate.js
@@ -2,13 +2,13 @@
 import en from './en.json';
 import az from './az.json';
 import fr from './fr.json';
-import de from './de.json';
+// import de from './de.json';
 
 const translations = {
   en,
   az,
   fr,
-  de,
+  // de,
 };
 
 export const translate = (key, locale = 'en') => {
