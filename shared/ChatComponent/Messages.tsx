14dbe56344bb3724d360a34a8cff25d9bcdd3724
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-06-14 16:38:46 +0400

    chat

---
+++ shared/ChatComponent/Messages.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Messages() {
+  const [messages, setMessages] = useState([]);
+
+  return (
+    <div>Messages</div>
+  )
+}
+
+export default Messages
