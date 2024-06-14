2978e1d81f542d4126a6f06f24771a7a76be4155
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-06-14 16:40:17 +0400

    bugResolved

--- shared/ChatComponent/Messages.tsx
+++ shared/ChatComponent/Messages.tsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { db } from '@/server/configs/firebase';
+
 
 function Messages() {
   const [messages, setMessages] = useState([]);
 
+//   useEffect(() => {
+//     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+//       doc.exists() && setMessages(doc.data().messages);
+//     });
+
+//     return () => {
+//       unSub();
+//     };
+//   }, [data.chatId]);
   return (
     <div>Messages</div>
   )
